32df1145f3272896f390bab380018c2054df715b
EliasBouillanne <elias.bougj@hotmail.fr>
2019-03-12 19:54:23 +0100

    run

--- Base_Flask/run.py
+++ Base_Flask/run.py
@@ -1,8 +1,8 @@
 # Imports
 from flask import Flask
-import views#, forms
+import views
 
 main_app = views.app
 
 if __name__ == '__main__':
-    main_app.run(debug=True, port=2745) 
+    main_app.run(debug=True, port=2745)
