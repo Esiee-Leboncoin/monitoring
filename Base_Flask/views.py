a08327215eba927d6a44927b5c69ccdfd80e4cb7
EliasBouillanne <elias.bougj@hotmail.fr>
2019-03-13 11:48:38 +0100

    Utilisation de la classe mongoDB pour chercher les users

--- Base_Flask/views.py
+++ Base_Flask/views.py
@@ -1,17 +1,70 @@
 # Imports
 from flask import Flask, render_template, url_for, request, session, redirect
 
-import matplotlib.pyplot as plt
-
+import config
 from utils import bdd
 
-# Configuration de l'application Flask avec la base de données Mongo
+# Initialisation et chargment du fichier Config
 app = Flask(__name__)
-
-app.config.update(SECRET_KEY='yoursecretkey')
+app.config.from_object(config)
 
 #Initialisation de la base de donnée
 mongo = bdd.MongoDB("database_pipeline")
+
+# Route principale de l'application Flask
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if 'username' in session:
+        return render_template('main.html',
+            username = session['username'],
+            all_last_scores = mongo.find_all_last(last=True))
+
+    return render_template('index.html')
+
+# Route d'inscription des utilisateurs
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        existing_user = mongo.find("users", {'name' : request.form['username']})
+
+        if existing_user is None:
+            mongo.insert("users", {'name' : request.form['username'], 'password' : request.form['pass']})
+            session['username'] = request.form['username']
+            return redirect('/')
+
+        return redirect('/error_register')
+
+    return render_template('register.html')
+
+# Route de connexion des utilisateurs
+@app.route('/login',methods=['POST'])
+def login():
+    login_user = mongo.find("users", {'name' : request.form['username']})[0]
+
+    if login_user :
+        if (request.form['pass'].encode('utf-8') == login_user['password'].encode('utf-8')):
+            session['username'] = request.form['username']
+            return redirect('/')
+
+    return redirect('/error_login')
+
+# Route d'erreur d'inscription
+@app.route('/error_register')
+def error_register():
+    return render_template('error_register.html')
+
+# Route d'erreur de connexion
+@app.route('/error_login')
+def error_login():
+    return render_template('error_login.html')
+
+# Route de déconnexion de l'utilisateur
+@app.route('/deco')
+def deconnection():
+    del session['username']
+    return redirect('/')
+
+import matplotlib.pyplot as plt
 
 def create_histo(collec_pipe_name):
 
@@ -48,57 +101,3 @@ def get_graph(date_liste, indice_liste, collec_pipe_name, type_indice):
     ax.plot(date_liste, indice_liste)
     fig.savefig("static/" + str(collec_pipe_name) + "_" + str(type_indice) + ".png")
     return graph
-
-# Route principale de l'application Flask
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if 'username' in session:
-        return render_template('main.html', username = session['username'], all_last_scores = mongo.find_all("collection_pipeline_1", last=True))
-
-    return render_template('index.html')
-
-
-# Route d'inscription des utilisateurs
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
-
-        if existing_user is None:
-            users.insert({'name' : request.form['username'], 'password' : request.form['pass']})
-            session['username'] = request.form['username']
-            return redirect('/')
-
-        return redirect('/error_register')
-
-    return render_template('register.html')
-
-# Route de connexion des utilisateurs
-@app.route('/login',methods=['POST'])
-def login():
-    users = mongo.db.users
-    login_user = users.find_one({'name' : request.form['username']})
-
-    if login_user :
-        if (request.form['pass'].encode('utf-8') == login_user['password'].encode('utf-8')):
-            session['username'] = request.form['username']
-            return redirect('/')
-
-    return redirect('/error_login')
-
-# Route d'erreur d'inscription
-@app.route('/error_register')
-def error_register():
-    return render_template('error_register.html')
-
-# Route d'erreur de connexion
-@app.route('/error_login')
-def error_login():
-    return render_template('error_login.html')
-
-# Route de déconnexion de l'utilisateur
-@app.route('/deco')
-def deconnection():
-    del session['username']
-    return redirect('/')
