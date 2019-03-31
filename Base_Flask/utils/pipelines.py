a9702326a355aca0432d265586626af31af531e4
EliasBouillanne <elias.bougj@hotmail.fr>
2019-03-31 17:54:34 +0200

    Add full regression monitoring and insert in bdd

--- Base_Flask/utils/pipelines.py
+++ Base_Flask/utils/pipelines.py
@@ -1,7 +1,31 @@
-import importlib
-import matplotlib.pyplot as plt
+from utils import bdd, graph
 
-def load_all_data(path):
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_validate
+
+
+
+def add_metadata_property(obj, name):
+    '''
+        Permet d'ajouter un atribut à un objet existant
+    '''
+    setattr(obj, "name", name)
+
+#add_metadata_property(pipe_elias, 'pipe_elias')
+
+def get_pipeline(pipe_name):
+    '''
+        Selectionne et renvoie la pipeline selectionnée
+    '''
+
+
+def load_data(path):
     '''
         Ouvre et lis le fichier passer en parmètre, reconnais seulement les types de
         fichier supportés par pandas : CSV, JSON, HTML, Local clipboard, MS Excel,
@@ -70,16 +94,20 @@ def descript_df(dataframe):
     display = corr
     return display
 
-def compute_performance(pipeline, modele, df):
+def compute_performance(pipeline, modele, df,  features, target, n, BDD=True):
     '''
         Permet d'appeler les bons indicateurs de performances et de récupérer
         les informations utiles selon la pipeline passé en paramètre. Retourne les
         performances associées.
 
         :params:
-            pipeline : sortie de la pipeline
+            pipeline : object de type pipeline
             modele : type du modèle utilisé : régression/classification)
-            base : tuple des bases d'apprentissage et de test
+            features : colonnent à utiliser pour la régréssion
+            target : colonne à prédire
+            n : nombre de bootstrap pour le calcul des intervalles de confiance
+            BDD : booléen, pour True les résultats sont stockés dans la BDD pour
+                  False ils ne sont pas sauvegardés
 
         :type params:
             bool_type_modele: boolean
@@ -87,11 +115,25 @@ def compute_performance(pipeline, modele, df):
 
         :return: les performances des différents indicateurs et graphiques
     '''
-    if (modele == "régression") :
+    if (modele == "regression") :
         print("Choix du type d'estimateur : Régression \n")
-        perfs = compute_regression(pipeline, df)
-        return perfs
-        # poster les scores dans la database
+        result = compute_regression(pipeline, df, features, target, n)
+        #compute_performance(pipe_elias, "regression", data, ["age_range", "head_size"], ["brain_weight"], 100)
+
+    elif(modele == "classification"):
+        print("Choix du type d'estimateur : Classification \n")
+        result = compute_classification(pipeline, df)
+
+    else:
+        return -1
+
+    if BDD == True:
+        result['Time'] = datetime.datetime.now()
+        result["_id"] = pipeline.name + "." + str(result['Time'])
+        mongo = MongoDB("database_pipeline")
+        mongo.insert_one(pipeline.name, result)
+
+    return result
 
     elif(modele == "classification"):
         print("Choix du type d'estimateur : Classification \n")
@@ -102,37 +144,77 @@ def compute_performance(pipeline, modele, df):
     else:
         return -1
 
-def indic_perform_reg_1(pipeline, df):
+def compute_regression(pipeline, df, features, target, n):
     '''
-        Permet d'obtenir les résultats des indicateurs de performances d'une régression, et retourne ces derniers.
+        Permet d'obtenir les résultats des indicateurs de performances d'une régression par crossvalidation,
+        et retourne ces derniers.
 
         :params:
-            pipeline : sortie de la pipeline
-            data : tuple des bases d'apprentissage et de test
+            pipeline : object de type pipeline
+            df : tuple des bases d'apprentissage et de test
+            features : colonnent à utiliser pour la régréssion
+            target : colonne à prédire
+            n : nombre de bootstrap pour le calcul des intervalles de confiance
+            BDD : booléen, pour True les résultats sont stockés dans la BDD pour
+                  False ils ne sont pas sauvegardés
+        :return:
+            r2 : score R2
+            variance : variance gloable expliquée par le modèle
+            intervalle_10 : une liste de 10 intervalles de confiances pris dans la list triés
+                            des intervalles de confiances.
+            intervalle_mean : moyenne de tous les intervalles de confiance
+    '''
+
+    # Calcul interval de confiance par bootstrap
+    pred = bootstrap(pipe_test, df, features, target, n)
+    inter_every_x = [np.mean(pred[i]) - 2 * np.std(pred[i]) for i in pred.keys()]
+    step = int(len(df)/10)
+    intervalle_10 = [inter_every_x[i] for i in [step, step*2, step*3, step*4, step*5, step*6,
+                                            step*7, step*8, step*9, step*10]]
+    mean_inter = np.mean(inter_every_x)
+
+    # Calcul de la variance globale expliqué et r2 par Cross-Validation
+    scoring = {"variance" : "explained_variance",
+          "r2" : "r2"}
+    result = cross_validate(pipeline, data[features], data[target], cv=7, scoring=scoring)
+    r2 = mean(result["test_r2"])
+    variance = np.mean(result["test_variance"])
+
+    result = {"r2": r2, "variance": variance, "intervalle_10" : intervalle_10, "intervalle_mean" : mean_inter}
+
+    return result
+
+def bootstrap(pipeline, data, features, target, n):
+    '''
+        Effectue un boostrap de la pipeline sur les données passées en paramètre.
+
+        :params:
+            pipeline : pipeline
+            data : dataframe
+            features : colonnent à utiliser pour la régréssion
+            target : colonne à prédire
+            n : nombres boostrap à faire
 
         :return:
-            rmse : score RMSE
-            r2 : score R²
-            cross_val : score de cross-validation
+            pred : dictionnaire contenant les valeurs bootstrap sous forme de liste pour chaque echantillons.
     '''
-    # Pré-process ?
-    df_pred = pipeline.predict(df)
+    length = len(data)
+    keys = range(length)
+    pred = {key: list() for key in keys}
 
-    # Calcul du RMSE
-    #rmse = sum(sqrt(df-pred))
+    for b in range(n):
+        #Random choice
+        np.random.seed(b)
+        index = np.random.choice(range(length), int(length/0.7))
+        index_test = data.index.difference(index)
+        train = data.loc[index]
+        test = data.loc[index_test]
 
-    compute_R2(test[,c("Energie")], df_pred)
+        #Fit des données
+        pipeline.fit(train[features], train[target])
 
-    # Calcul du R²
-    ajusted_r2 = r2_score(df, pred)
+        #Prédiction et score sur la base de test
+        for i, p in zip(index_test, pipeline.predict(test[features])):
+            pred[i].append(p[0])
 
-    # Cross-Validation
-    #cross_val = np.mean(cross_val_score(pipeline, df, df, cv=5))
-
-    return rmse, r2
-
-
-def get_pipeline(pipe_name):
-    '''
-        Selectionne et renvoie la pipeline selectionnée
-    '''
+    return pred
