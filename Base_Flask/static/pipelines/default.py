6c8247835ae13f62cf1f5533105c5b32412650f1
EliasBouillanne <elias.bougj@hotmail.fr>
2019-04-16 21:48:54 +0200

    defautl

--- Base_Flask/static/pipelines/default.py
+++ Base_Flask/static/pipelines/default.py
@@ -1,11 +1,14 @@
-from sklearn.pipeline import Pipeline
-
-#Ajouter tous les imports necessaires
-from sklearn.preprocessing import StandardScaler
-from sklearn import neighbors
-
-#Importer sa pipeline sans modifier le nom
-pipeline = Pipeline([
-    ('features', StandardScaler()),
-    ('estimator', neighbors.KNeighborsRegressor())
-])
+#Ajouter tous les imports necessaires
+from sklearn.pipeline import Pipeline
+
+from sklearn.preprocessing import StandardScaler
+from sklearn import neighbors
+
+
+
+#Importer sa pipeline sans modifier le nom
+
+pipeline = Pipeline([
+    ('features', StandardScaler()),
+	('estimator', neighbors.KNeighborsRegressor())
+])
