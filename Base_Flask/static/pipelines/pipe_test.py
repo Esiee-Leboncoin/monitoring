05ea5fef9992461cd607d2b9b9c26a9425c336c9
Steven POULAIN <poulains@pc5301-test.esiee.fr>
2019-04-16 12:42:29 +0200

    No-graph button update

--- Base_Flask/static/pipelines/pipe_test.py
+++
@@ -1,9 +0,0 @@
-from sklearn.pipeline import Pipeline
-
-from sklearn.preprocessing import StandardScaler
-from sklearn import neighbors
-
-name_pipeline = Pipeline([
-    ('features', StandardScaler()),
-    ('estimator', neighbors.KNeighborsRegressor())
-])
